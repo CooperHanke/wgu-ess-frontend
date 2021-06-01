9e6a9fdcf8107cc5e0f4e84655bb8e55d5e8a222
Cooper Hanke <cooper.hanke@gmail.com>
2021-06-01 22:12:35 +0000

    Mobile touchup

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,48 +1,51 @@
 <template>
   <v-navigation-drawer app clipped permanent :mini-variant="mini">
     <v-list dense nav>
-      <!-- using a regular list, giving out regular links first -->
-      <v-list-item
-        v-for="item in menuItems"
-        :key="item.title"
-        link
-        @click="switchPageView(item)"
-      >
-        <v-list-item-icon>
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-list-item-icon>
-
-        <v-list-item-content>
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-
-      <!-- now, here we have the special reports menu -->
-      <v-list-group prepend-icon="mdi-file-chart">
-        <template v-slot:activator>
-          <v-list-item-title>Reports</v-list-item-title>
-        </template>
-
+      <v-list-item-group>
+        <!-- using a regular list, giving out regular links first -->
         <v-list-item
-          v-for="item in reportItems"
+          v-for="item in menuItems"
           :key="item.title"
           link
-          @click="switchReportView(item)"
+          @click="switchPageView(item)"
+          color="primary"
         >
-        <v-list-item-action>
-              <v-icon></v-icon>
-            </v-list-item-action>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
           <v-list-item-content>
-            <v-list-item-title v-text="item.title"></v-list-item-title>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-      </v-list-group>
 
-      <v-divider class="mx-4"></v-divider>
+        <!-- now, here we have the special reports menu -->
+        <v-list-group prepend-icon="mdi-file-chart">
+          <template v-slot:activator>
+            <v-list-item-title>Reports</v-list-item-title>
+          </template>
+
+          <v-list-item
+            v-for="item in reportItems"
+            :key="item.title"
+            link
+            @click="switchReportView(item)"
+          >
+            <v-list-item-action>
+              <v-icon></v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.title"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-group>
+
+        <v-divider class="mx-4"></v-divider>
+      </v-list-item-group>
     </v-list>
 
     <template v-slot:append>
-      <div class="text-center pa-1" v-if="mini">
+      <div class="text-center pa-1" v-if="mini" @click="logout">
         <v-btn color="primary" rounded small>
           <v-icon> mdi-logout </v-icon>
         </v-btn>
@@ -67,16 +70,18 @@ export default {
       return this.items.filter((item) => !item.availableReports);
     },
     reportItems() {
-      const reportItem = this.items.find(item => item.availableReports);
+      const reportItem = this.items.find((item) => item.availableReports);
       if (this.userType === "manager") {
-        return reportItem.availableReports
+        return reportItem.availableReports;
       } else {
-        return reportItem.availableReports.filter(r => r.userGroup === this.userType)
+        return reportItem.availableReports.filter(
+          (r) => r.userGroup === this.userType
+        );
       }
     },
     userType() {
-      return this.$store.state.user.type
-    }
+      return this.$store.state.user.type;
+    },
   },
   data() {
     return {
@@ -90,17 +95,17 @@ export default {
             {
               title: "Appointments By Month",
               location: "appointments-by-month",
-              userGroup: "standard"
+              userGroup: "standard",
             },
             {
               title: "Total Appointments By Contact",
               location: "total-appointments-by-contact",
-              userGroup: "standard"
+              userGroup: "standard",
             },
             {
               title: "User Schedules",
               location: "user-schedules",
-              userGroup: "manager"
+              userGroup: "manager",
             },
           ],
         },
@@ -111,21 +116,21 @@ export default {
     switchPageView(item) {
       if (!item.location) {
         this.$store.commit("setActivePage", this.formatLink(item.title));
-      } else return
+      } else return;
     },
     switchReportView(item) {
       if (item.location) {
-        this.$store.commit("setActivePage", 'reports')
-        this.$store.commit("setReportPage", item.location)
-      } else return
+        this.$store.commit("setActivePage", "reports");
+        this.$store.commit("setReportPage", item.location);
+      } else return;
     },
     formatLink(link) {
       return link.toLowerCase();
     },
     logout() {
-      this.$store.commit("logout")
-      this.$router.push({ name: 'Login' })
-    }
+      this.$store.commit("logout");
+      this.$router.push({ name: "Login" });
+    },
   },
 };
 </script>
