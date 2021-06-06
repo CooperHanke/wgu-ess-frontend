f814637cd2af90ecffd410d9b4176768d08d22bc
Cooper Hanke <cooper.hanke@gmail.com>
2021-06-05 21:45:18 -0500

    created initial actions for user management page

--- src/components/user-management/UserManagement.vue
+++ src/components/user-management/UserManagement.vue
@@ -39,31 +39,31 @@
           <v-icon medium class="mr-2" @click="resetPassword(item.username)">
             mdi-lock-reset
           </v-icon>
-          <v-icon medium @click="disableUser(item.username)"> mdi-account-lock </v-icon>
+          <v-icon medium :color="isLockedIcon(item)" @click="disableUser(item)"> {{ isLocked(item) ? 'mdi-account-lock' : 'mdi-account-lock-outline' }} </v-icon>
         </template>
       </v-data-table>
     </v-sheet>
-    <!-- <v-dialog v-model="dialogDelete" max-width="500px">
+    <v-dialog v-model="dialogDisable" max-width="800">
       <v-card>
-        <v-card-title class="headline"
-          >Are you sure you want to delete this item?</v-card-title
-        >
+        <v-card-title>Are you sure you want to disable this {{user.username}}?</v-card-title>
+        <v-card-text>You can unlock again in the future</v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-          <v-btn color="blue darken-1" text @click="deleteContactConfirm"
+          <v-btn color="blue darken-1" text >Cancel</v-btn>
+          <v-btn color="blue darken-1" text
             >OK</v-btn
           >
           <v-spacer></v-spacer>
         </v-card-actions>
       </v-card>
-    </v-dialog> -->
+    </v-dialog>
   </div>
 </template>
 
 <script>
 export default {
   data: () => ({
+    dialogDisable: false,
     headers: [
       {
         text: "User's Full Name",
@@ -121,7 +121,8 @@ export default {
         userType: "standard",
         userStatus: "disabled"
       }
-    ]
+    ],
+    user: {}
   }),
   methods: {
     getColor( userStatus ) {
@@ -141,8 +142,20 @@ export default {
     resetPassword(username) {
       console.log(`would have reset password for ${username}`)
     },
-    disableUser(username) {
-      console.log(`would have disabled account for ${username}`)
+    disableUser(user) {
+      console.log(`would have disabled account for ${user.username}`)
+      this.user = user
+      this.dialogDisable = true
+    },
+    isLocked(user) {
+      return user.userStatus === 'locked'
+    },
+    isLockedIcon(user) {
+      if (this.isLocked(user)) {
+        return 'red'
+      } else {
+        return 'green'
+      }
     }
   }
 }
