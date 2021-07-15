ca8cf6f0d8f4e9bd42bdaf67cc90b7344ee5fdd2
Cooper Hanke <cooper.hanke@gmail.com>
2021-07-15 12:16:03 -0500

    added the checkbox to enable appointment reminders

--- src/components/appointments/AppointmentFormDialog.vue
+++ src/components/appointments/AppointmentFormDialog.vue
@@ -17,7 +17,11 @@
             <v-form ref="appointmentForm" v-model="valid">
               <v-row>
                 <v-col cols="6">
-                  <v-text-field v-model="title" label="Title" :rules="textRules"></v-text-field>
+                  <v-text-field
+                    v-model="title"
+                    label="Title"
+                    :rules="textRules"
+                  ></v-text-field>
                 </v-col>
 
                 <v-col cols="6">
@@ -41,11 +45,19 @@
                 </v-col>
 
                 <v-col cols="12" sm="6" md="4">
-                  <v-text-field v-model="type" label="Type" :rules="textRules"></v-text-field>
+                  <v-text-field
+                    v-model="type"
+                    label="Type"
+                    :rules="textRules"
+                  ></v-text-field>
                 </v-col>
 
                 <v-col cols="12" sm="6" md="4">
-                  <v-text-field v-model="url" label="URL" :rules="urlRules"></v-text-field>
+                  <v-text-field
+                    v-model="url"
+                    label="URL"
+                    :rules="urlRules"
+                  ></v-text-field>
                 </v-col>
 
                 <v-col cols="6">
@@ -118,6 +130,22 @@
                   ></v-text-field>
                 </v-col>
               </v-row>
+
+              <v-row>
+                <v-col cols="4">
+                  <v-checkbox
+                    v-model="enableReminder"
+                    label="Enable Reminder?"
+                  ></v-checkbox>
+                </v-col>
+                <v-col v-show="this.enableReminder" cols="4">
+                  <v-text-field
+                    label="Reminder time on start date?"
+                    value=""
+                    type="time"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
             </v-form>
           </v-container>
         </v-card-text>
@@ -125,7 +153,9 @@
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn color="blue darken-1" text @click="close"> Cancel </v-btn>
-          <v-btn color="blue darken-1" text @click="save" :disabled="!valid"> Save </v-btn>
+          <v-btn color="blue darken-1" text @click="save" :disabled="!valid">
+            Save
+          </v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -133,8 +163,8 @@
 </template>
 
 <script>
-import ContactSelectionForAppointments from "@/components/appointments/ContactSelectionForAppointments.vue"
-import moment from 'moment'
+import ContactSelectionForAppointments from "@/components/appointments/ContactSelectionForAppointments.vue";
+import moment from "moment";
 
 export default {
   components: {
@@ -156,48 +186,57 @@ export default {
     },
     loading: {
       get() {
-        return this.$store.getters['appointments/appointmentLoadingForEdit']
-      }
+        return this.$store.getters["appointments/appointmentLoadingForEdit"];
+      },
     },
     appointment: {
       get() {
-        return this.$store.getters["appointments/appointment"]
+        return this.$store.getters["appointments/appointment"];
       },
     },
     startDateTime() {
-      return new Date(this.startDate + ' ' + this.startTime).toJSON()
+      return new Date(this.startDate + " " + this.startTime).toJSON();
     },
     endDateTime() {
-      return new Date(this.endDate + ' ' + this.endTime).toJSON()
+      return new Date(this.endDate + " " + this.endTime).toJSON();
     },
     hasContactId() {
-      return this.$store.getters["appointments/contactId"] !== null || this.$store.getters["appointments/contactId"] !== ''
-    }
+      return (
+        this.$store.getters["appointments/contactId"] !== null ||
+        this.$store.getters["appointments/contactId"] !== ""
+      );
+    },
   },
 
   data() {
     return {
-      title: '',
-      description: '',
-      location: '',
-      url: '',
-      type: '',
-      startDate: '',
-      startTime: '',
-      endDate: '',
-      endTime: '',
+      title: "",
+      description: "",
+      location: "",
+      url: "",
+      type: "",
+      startDate: "",
+      startTime: "",
+      endDate: "",
+      endTime: "",
       dialogDelete: false,
       startDatePicker: false,
       endDatePicker: false,
-      urlRules: [(v) => /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_.~#?&//=]*)/g.test(v) || 'Please enter a valid URL, including the protocol (http/https)'],
+      enableReminder: false,
+      urlRules: [
+        (v) =>
+          /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_.~#?&//=]*)/g.test(
+            v
+          ) || "Please enter a valid URL, including the protocol (http/https)",
+      ],
       textRules: [
         (v) => !!v || "This field is required",
-        (v) => /^[a-zA-Z0-9-\s]+$/g.test(v) || "Uppercase charaters, spaces, dashes, and numbers only in this field"
+        (v) =>
+          /^[a-zA-Z0-9-\s]+$/g.test(v) ||
+          "Uppercase charaters, spaces, dashes, and numbers only in this field",
       ],
-      filled: [
-        (v) => !!v || "This field is required"
-      ],
-      valid: false
+      filled: [(v) => !!v || "This field is required"],
+      valid: false,
     };
   },
 
@@ -229,11 +268,14 @@ export default {
         type: this.type,
         startDate: this.startDateTime,
         endDate: this.endDateTime,
-        contactId: this.$store.getters['appointments/contactId'],
-        userId: this.$store.getters['auth/userId']
-      }
-      if (this.$store.getters['appointments/appointmentId']) {
-        this.$store.dispatch("appointments/saveExistingAppointment", appointment)
+        contactId: this.$store.getters["appointments/contactId"],
+        userId: this.$store.getters["auth/userId"],
+      };
+      if (this.$store.getters["appointments/appointmentId"]) {
+        this.$store.dispatch(
+          "appointments/saveExistingAppointment",
+          appointment
+        );
       } else {
         this.$store.dispatch("appointments/saveNewAppointment", appointment);
       }
@@ -241,25 +283,29 @@ export default {
     },
   },
   watch: {
-    appointment: function() {
-      if (this.$store.getters['appointments/appointmentId']) {
-        this.title = this.appointment.title,
-        this.description = this.appointment.description,
-        this.location = this.appointment.location,
-        this.url = this.appointment.url,
-        this.type = this.appointment.type,
-        this.startDate = this.appointment.startDate,
-        this.startTime = moment(this.appointment.startTime, ['h:mm A']).format('HH:mm'),
-        this.endDate = this.appointment.endDate,
-        this.endTime = moment(this.appointment.endTime, ['h:mm A']).format('HH:mm')
+    appointment: function () {
+      if (this.$store.getters["appointments/appointmentId"]) {
+        (this.title = this.appointment.title),
+          (this.description = this.appointment.description),
+          (this.location = this.appointment.location),
+          (this.url = this.appointment.url),
+          (this.type = this.appointment.type),
+          (this.startDate = this.appointment.startDate),
+          (this.startTime = moment(this.appointment.startTime, [
+            "h:mm A",
+          ]).format("HH:mm")),
+          (this.endDate = this.appointment.endDate),
+          (this.endTime = moment(this.appointment.endTime, ["h:mm A"]).format(
+            "HH:mm"
+          ));
       }
     },
-    hasContactId: function() {
-      console.log(this.contactId)
-      if (this.contactId === null || this.contactId === '') {
-        this.valid = false
+    hasContactId: function () {
+      console.log(this.contactId);
+      if (this.contactId === null || this.contactId === "") {
+        this.valid = false;
       }
-    }
-  }
+    },
+  },
 };
 </script>
