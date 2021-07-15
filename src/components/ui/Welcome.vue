9d40fcff6d1057d6b8e2be078d125f3203031c55
Cooper Hanke <cooper.hanke@gmail.com>
2021-07-15 09:11:27 -0500

    added welcome screen upon user logging in, and added custom nginx config to forward all traffic to app at endpoint

---
+++ src/components/ui/Welcome.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+    <v-alert
+      text
+      type="info"
+      border="left"
+      prominent
+    >
+      Welcome to the WGU Enhanced Scheduling System, @{{ userName }}! Choose any menu selection on the left to get started.
+    </v-alert>
+  </v-container>
+</template>
+
+<script>
+export default {
+  computed: {
+    userName() {
+      return this.$store.getters['auth/userName']
+    }
+  }
+}
+</script>
